167abe12a998f792e767b7c95534796c989faf92
Gabriel Couto <gabriel.justino@dsgtechnology.com.br>
2026-09-14 12:00:21 -0300

    refat: router en us all vars port dev 4242

--- vuejszin/src/main.js
+++ vuejszin/src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import EditorView from './views/EditorView.vue'
-import CertidaoPublicaView from './views/CertidaoPublicaView.vue'
-import './styles.css'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import EditorView from "./views/EditorView.vue";
+import PublicCertificateView from "./views/PublicCertificateView.vue";
+import "./styles.css";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: EditorView },
-    { path: '/c/:slug', component: CertidaoPublicaView },
+    { path: "/", component: EditorView },
+    { path: "/c/:publicId", component: PublicCertificateView },
   ],
-})
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
